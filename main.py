be23515f167a564afbbb72cd7f0113979784a0c1
SmellyCat <smellycat2014@foxmail.com>
2017-03-13 11:47:27 +0800

    更改 整合短信发送和卡口信息类，helper_sms,helper_kakou2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import time
 import datetime
 import json
@@ -6,93 +7,52 @@ import json
 import arrow
 import requests
 
-import helper
+from helper_kakou2 import Kakou
+from helper_sms import SMS
 from ini_conf import MyIni
+from my_logger import *
 
 
-class SMSSender:
+debug_logging(u'/home/logs/error.log')
+logger = logging.getLogger('root')
+
+
+class BKCPAlarm(object):
     def __init__(self):
-        self.myini = MyIni()
-        self.sms_conf = self.myini.get_sms()
-        self.kakou_ini = {
-            'host': '10.47.187.165',
-            'port': 80
-        }
-        self.id_flag = self.sms_conf['id_flag']
-        self.step = 100
+        self.my_ini = MyIni()
 
-        self.kakou_status = False
+        self.sms = SMS(**self.my_ini.get_sms())
+        self.kakou = Kakou(**self.my_ini.get_kakou())
+        #self.id_flag = self.sms_conf['id_flag']
+        self.step = 1000
 
         # 布控车牌字典形如 {'粤LXX266': {'kkdd': '东江大桥卡口',
         # 'jgsj': <Arrow [2016-03-04T09:39:45.738000+08:00]>}}
         self.bkcp_dict = {}
+        # 布控车辆信息字典
+        self.bkcp_info_dict = {}
+        # 城市名
+        self.cityname = self.my_ini.get_kakou()['cityname']
         
     def __del__(self):
-        del self.myini
-
-    def get_cltxmaxid(self):
-        url = 'http://%s:%s/rest_hz_kakou/index.php/%s/kakou/cltxmaxid' % (
-            self.kakou_ini['host'], self.kakou_ini['port'], 'hcq')
-        try:
-            r = requests.get(url)
-            if r.status_code == 200:
-                return json.loads(r.text)
-            else:
-                self.kakou_status = False
-                raise Exception('url: %s, status: %s, %s' % (
-                    url, r.status_code, r.text))
-        except Exception as e:
-            self.kakou_status = False
-            raise
-
-    def get_cltxs(self, _id, last_id):
-        url = 'http://%s:%s/rest_hz_kakou/index.php/%s/kakou/cltxs/%s/%s' % (
-            self.kakou_ini['host'], self.kakou_ini['port'], 'hcq', _id, last_id)
-        try:
-            r = requests.get(url)
-            if r.status_code == 200:
-                return json.loads(r.text)
-            else:
-                self.kakou_status = False
-                raise Exception('url: %s, status: %s, %s' % (
-                    url, r.status_code, r.text))
-        except Exception as e:
-            self.kakou_status = False
-            raise
-
-    def get_bkcp_by_hphm(self, hphm):
-        url = u'http://%s:%s/rest_hz_kakou/index.php/%s/kakou/bkcp?q=%s' % (
-            self.kakou_ini['host'], self.kakou_ini['port'], 'hcq', hphm)
-        try:
-            r = requests.get(url)
-            if r.status_code == 200:
-                return json.loads(r.text)
-            else:
-                self.kakou_status = False
-                raise Exception('url: %s, status: %s, %s' % (
-                    url, r.status_code, r.text))
-        except Exception as e:
-            self.kakou_status = False
-            raise
+        del self.my_ini
 
     def send_sms(self, cltx):
-        bkcp = self.get_bkcp_by_hphm(cltx['hphm'])
-        if bkcp['total_count'] >= 1:
-            try:
-                print u'BKCP: {0}, {1}'.format(cltx['hphm'], cltx['jgsj'])
-                memo = ''
-                if bkcp['items'][0]['memo'] != None:
-                    memo = bkcp['items'][0]['memo']
-                data = {
-                    'mobiles': bkcp['items'][0]['mobiles'].split(','),
-                    'content': u'[惠城区卡口报警]%s,%s,%s,%s.(%s)' % (
-                        cltx['jgsj'], cltx['kkdd'], cltx['fxbh'], cltx['hphm'],
-                        memo)
-                }
-                #print 'data: %s' % data
-                helper.sms_post(data)
-            except Exception as e:
-                print (e)
+        """发送短信"""
+	bkcp = self.kakou.get_bkcp(cltx['hphm'])
+        if bkcp is None:
+	    return
+        try:
+            logger.info(u'BKCP: {0}, {1}'.format(cltx['hphm'], cltx['jgsj']))
+            if bkcp['memo'] is None:
+                bkcp['memo'] = ''
+            mobiles = bkcp['mobiles']
+            content = u'[{0}卡口报警]{1},{2},{3},{4}.({5})'.format(
+                self.cityname, cltx['jgsj'], cltx['kkdd'], cltx['fxbh'],
+                cltx['hphm'], bkcp['memo'])
+            self.sms.sms_send(content, mobiles)
+        except Exception as e:
+            logger.error(e)
 
     def check_is_repeat(self, hphm, kkdd, jgsj):
         """检查是否重复发送"""
@@ -112,53 +72,41 @@ class SMSSender:
             raise
         finally:
             self.bkcp_dict[hphm] = {'kkdd': kkdd, 'jgsj': t}
-        
-    def loop_get_data(self):
-        self.id_flag = self.get_cltxmaxid()['maxid'] - 100
+
+    def check_bkcp(self):
+	"""检查布控车牌"""
+        maxid = self.kakou.get_maxid()
+	#print 'self.id_flag = %s' % self.id_flag
+        if self.id_flag < maxid:
+            kakou_info = self.kakou.get_kakou(
+                self.id_flag+1, self.id_flag+self.step)
+            if kakou_info['total_count'] > 0:
+                for i in kakou_info['items']:
+                    if i['clbj'] in set(['B', 'L']):
+                        if not self.check_is_repeat(
+                            i['hphm'], i['kkdd'], i['jgsj']):
+                            self.send_sms(i)
+            if self.id_flag+self.step < maxid:
+                self.id_flag += self.step
+            elif kakou_info['total_count'] > 0:
+                self.id_flag = kakou_info['items'][-1]['id']
+            time.sleep(0.5)
+        else:
+            time.sleep(0.5)
+
+    def run(self):
+	ini_flag = False
         while 1:
             try:
-                maxid = self.get_cltxmaxid()['maxid']
-                if self.id_flag < maxid:
-                    #print self.id_flag
-                    cltx_dict = self.get_cltxs(
-                        self.id_flag, self.id_flag+self.step)
-                    if cltx_dict['total_count'] > 0:
-                        for i in cltx_dict['items']:
-                            if i['clbj'] in set(['B', 'L']):
-                                #print 'bkcp:%s,%s'%(i['hphm'], i['jgsj'])
-                                #self.send_sms(i)
-                                if not self.check_is_repeat(
-                                    i['hphm'], i['kkdd'], i['jgsj']):
-                                    self.send_sms(i)
-                    if self.id_flag+self.step < maxid:
-                        #print 'test'
-                        self.id_flag += self.step
-                        #self.myini.set_sms(self.id_flag)
-                    elif cltx_dict['total_count'] > 0:
-                        #print 'my_id:%s' % (cltx_dict['items'][-1]['id'])
-                        self.id_flag = cltx_dict['items'][-1]['id']
-                        #self.myini.set_sms(self.id_flag)
-                    time.sleep(0.1)
-                else:
-                    time.sleep(1)
+		if ini_flag:
+		    self.check_bkcp()
+		else:
+		    self.id_flag = self.kakou.get_maxid()
+		    ini_flag = True
             except Exception as e:
-                print (e)
-                time.sleep(5)
-
-if __name__ == "__main__":
-##    print 'main'
-##    while 1:
-##        try:
-##            fd = SMSSender()
-##            fd.loop_get_data()
-##        except Exception as e:
-##            print e
-##            time.sleep(15)
-    fd = SMSSender()
-    fd.loop_get_data()
-    del fd
-    #s = SMSSender()
-    #s.get_bkcp_by_hphm(u'粤LXX266')
-    #del s
+                logger.error(e)
+                time.sleep(10)
+            finally:
+                time.sleep(1)
 
 
